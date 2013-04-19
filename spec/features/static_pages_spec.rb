4cf549858ae48c8caecb3247352560ea946f7047
Uwe Drechsel <uwedr@suse.com>
2013-04-19 14:03:14 +0200

    Fixing tests for Home and Help

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,19 +1,32 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
 
+  let(:base_title) { "Betaman" } 
+  describe "Home page" do
     it "should have the content 'Beta Program'" do
       visit '/static_pages/home'
       page.should have_content('Beta Program')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "#{base_title} | Home")
+    end
   end
 
   describe "Help page" do
-
     it "should have the content 'Innerweb WIKI'" do
       visit '/static_pages/help'
       page.should have_content('Innerweb WIKI')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "#{base_title} | Help")
+    end
   end
+
 end
