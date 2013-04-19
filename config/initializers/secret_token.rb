59fe20fad66efce67a56487f21f1fd73977f64d1
Uwe Drechsel <uwedr@suse.com>
2013-04-19 12:08:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betaman::Application.config.secret_token = 'ae0f6a29c3652484729493dcb757d8d90de60e7c875d9ed531dfaac8f84e3a66b9746e1117fab034bb0de23ee7e1ecfe1b220599426184892b96016d5fc7e039'
