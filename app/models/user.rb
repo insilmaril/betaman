12c2278300803210e360a11d31b1446ff4b1904b
Uwe Drechsel <uwedr@suse.com>
2013-08-09 11:47:55 +0200

    Added some methods to handle related accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,29 @@ class User < ActiveRecord::Base
     accounts.each { |a| ar << a.uid }
     ar
   end
+
+  def add_account(provider, uid)
+    if !self.has_account?(provider, uid)                                           
+      self.accounts.build(uid: uid)         
+    end                                                                            
+  end
+
+  def has_account?(provider, uid) 
+    self.accounts.any? do |account|
+      account.uid == uid && account.provider == provider
+    end 
+  end 
+
+  def self.find_by_account(provider, uid)
+    id = Account.find_by_provider_and_uid(provider, uid)
+
+    # as a fallback, try the given url with or without a '/' at the end 
+    if !id then
+      if url.last == '/'    # try again without a '/' at the end
+        id = Account.find_by_provider_and_uid("open_id", url[0...-1])
+      else
+        id = Account.find_by_provider_and_uid("open_id", "#{url}/")
+      end 
+    end
+  end
 end
