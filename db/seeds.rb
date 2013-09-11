4deefcfb80ba72f137700d6aaf7a9c933a3700e7
Uwe Drechsel <uwedr@suse.com>
2013-09-11 12:16:17 +0200

    Minor edits in preparing development DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,46 @@
+#
 # Create default roles
-r = Role.new(name: "Admin")
-r.save
-r = Role.new(name: "Employee")
-r.save
-r = Role.new(name: "Tester")
-r.save
+#
+role_admin = Role.new(name: "Admin")
+role_admin.save
+role_empl = Role.new(name: "Employee")
+role_empl.save
+role_tester = Role.new(name: "Tester")
+role_tester.save
+
+
+#
+# Create admin user
+#
+
+admin = User.create({first_name: 'Adam', last_name: 'Admin', email: 'uwe@insilmaril.de'} )
+admin.accounts << Account.new(uid: 'http://insilmaril.myopenid.com/')
+Role.all.each do |r|
+  admin.roles << r
+end
+admin.save
+
+#
+# Create some data for playing in development environment
+#
 
 if Rails.env == 'development'
 
-  User.create(
-    [
-      {first_name: 'Uwe', last_name: 'Drechsel', email: 'uwe@insilmaril.de'},
-      {first_name: 'Dagobert', last_name: 'Duck', email: 'test@insilmaril.de'}
-  ]
-  )
-
-  u = User.find(1)
-  u.admin = true
-  u.accounts << Account.new(uid: 'http://insilmaril.myopenid.com/')
-  u.save!
-
-  Role.all.each do |r|
-    u.roles << r
-  end
-
-  u = User.find(2)
+  # Create some test users
+  u = User.new
+  u.first_name = 'Eva'
+  u.last_name = 'Employee'
+  u.email = 'test@insilmaril.de'
+  u.roles << role_empl
   u.accounts << Account.new(uid: 'http://vym.myopenid.com/')
-  u.save!
+  u.save
 
   0.upto(20) do |n| 
     User.create( {first_name: n, last_name: 'User', email: "user_#{n}@company.com"})
   end
 
 
+  # Create some test betas
   betas = Beta.create(
     [
       {name: 'SUSE Cloud 2.0', begin: '2013-06-01', end: '2013-08-31'},
@@ -40,7 +48,7 @@ if Rails.env == 'development'
   ]
   )
 
-  # Create some relations
+  # Add some test betas to user
   User.find_by_email('uwe@insilmaril.de').betas << Beta.all
 end
 
