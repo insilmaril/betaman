f0d2c8f85686b52e8f826e194d8d8abdd44cd595
Uwe Drechsel <uwedr@suse.com>
2013-11-24 22:35:27 +0100

    Case insensitive search for company name

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < ActiveRecord::Base
   attr_accessible :name
   has_many :users
+
+  def self.find_by_name(name)
+    Company.where('lower(name) = ?', name.downcase ).first
+  end
 end
