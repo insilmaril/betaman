0449971212c92c87b54f697bcaa99e1cf8be71a5
Uwe Drechsel <uwedr@suse.com>
2014-01-09 11:32:03 +0100

    Add "dev" to page title, if running in development environment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,13 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Betaman"
+    env = ""
+    env = " (dev)" if Rails.env.development?
+
     if page_title.empty?
-      base_title
+      base_title + env
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} | #{page_title}" + env
     end
   end
 
