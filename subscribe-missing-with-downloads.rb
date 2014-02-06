93ceefd01ef4eba1fc77a7de504ecf9813888976
Uwe Drechsel <uwedr@suse.com>
2014-02-06 10:24:26 +0100

    Sync lists to internal after subscribing

--- subscribe-missing-with-downloads.rb
+++ subscribe-missing-with-downloads.rb
@@ -4,6 +4,8 @@ require 'betaadmin'
 require 'download_helper'
 
 begin
+  puts "Subscribing external users with downloads and internal users..."
+
   betas_with_downloads = DownloadHelper.find_betas_with_downloads
   betas_with_downloads.each do |b|
     puts "* #{b[:beta].name}"
@@ -13,7 +15,11 @@ begin
     n = 1
     users.each do |u|
       puts "    Subscribing (#{n}/#{users.count}): #{u.logname}"
+      b[:beta].list.subscribe(u)
       n += 1
     end
+    
+    puts "    Sync external list to internal..."
+    b[:beta].list.sync_to_intern
   end
 end
