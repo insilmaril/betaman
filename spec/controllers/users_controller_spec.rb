0d9788aa93baab3ce15e447184d6a5f5b8310dbc
Uwe Drechsel <uwedr@suse.com>
2014-02-06 17:01:24 +0100

    Fixed test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,7 @@ describe UsersController do
   describe "for non-logged-in users" do
     it "should redirect to login page" do
       get :index
-      response.should redirect_to '/session/login'
+      response.should redirect_to root_path
     end
   end
 
