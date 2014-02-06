762ee9f850e929eb69576a19a81011a194ad994c
Uwe Drechsel <uwedr@suse.com>
2014-02-06 10:23:45 +0100

    Revert "Sync lists to internal after subscribing"
    This reverts commit 3d581f9ba4298814bdcfad9d72ff46934e449fc5.

--- enable-missing-downloads.rb
+++ enable-missing-downloads.rb
@@ -4,33 +4,24 @@ require 'betaadmin'
 require 'download_helper'
 
 begin
-=begin
-  u = User.find 427
+  u = User.find 380
   u_login = ""
   u_login = u.login_name if !u.login_name.blank?
   u_company = ""
   u_company = u.company.name if u.company
   
-  b = Beta.find 2
+  b = Beta.find 1
   admin = BetaAdmin.new( b.novell_user, b.novell_pass, b.novell_id)
   admin.login
 
-  umail = "" 
-  if !u.alt_email.blank?
-    umail = u.alt_email
-  else
-    umail = u.email
-  end
-
-  puts "Trying to add #{umail}"
-  email_used = admin.add( {email: umail, elogin: u.login_name, company: u_company } )
+  email_used = admin.add( {email: u.email, elogin: u.login_name, company: u_company } )
   puts "Added: #{email_used}"
   if email_used != u.email
     u.alt_email = email_used
     u.save!
   end
+
   exit
-=end
 
   betas_with_downloads = DownloadHelper.find_betas_with_downloads
   betas_with_downloads.each do |b|
@@ -49,16 +40,9 @@ begin
       u_company = ""
       u_company = u.company.name if u.company
       
-      umail = "" 
-      if !u.alt_email.blank?
-        umail = u.alt_email
-      else
-        umail = u.email
-      end
-     # puts "Trying to add #{umail}"
-      email_used = admin.add( {email: umail, elogin: u.login_name, company: u_company } )
-      if email_used != umail
-        puts "Alternative email used: #{umail} -> #{email_used}"
+      email_used = admin.add( {email: u.email, elogin: u.login_name, company: u_company } )
+      if email_used != u.email
+        puts "Alternative email used: #{u.email} -> #{email_used}"
         u.alt_email = email_used
         u.save!
       end
