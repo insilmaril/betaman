9e52167daf4b19a230b143ba37727c4600c31e22
Uwe Drechsel <uwedr@suse.com>
2014-02-27 13:24:34 +0100

    Improved login process
    No openID required, but redirected directly to provider
    plus some eye candy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/store/filesystem'
   provider :open_id
   provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :open_id, :name => 'novell', :identifier => 'https://secure-www.novell.com/openid/user'
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
