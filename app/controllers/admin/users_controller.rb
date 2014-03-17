9f5e8bcee6c017a9a2a5cfb1e9f6c18791ff2c85
Uwe Drechsel <uwedr@suse.com>
2014-03-17 09:18:32 +0100

    Also set company for employees

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,11 +19,30 @@ class Admin::UsersController < ApplicationController
   def update_roles
     @users_employee_added = []
     @users_employee_dropped = []
+
+    suse = Company.where( name: "SUSE").first
+    if suse.nil?
+      flash[:error] = "Couldn't find company 'SUSE' in database"
+      redirect_to admin_path
+    end
+
+    novell = Company.where( name: "Novell").first
+    if novell.nil?
+      flash[:error] = "Couldn't find company 'Novell' in database"
+      redirect_to admin_path
+    end
+
     User.all.each do |u|
       if MailHelper.internal_domain?(u.email)
         # User is employee according to email domain
         if !u.employee?
           u.make_employee
+          if /suse\.(de|com|cz)$/.match(u.email)
+            u.company = suse
+          elsif /novell\.com$/.match(u.email)
+            u.company = suse
+          end
+
           u.save
           @users_employee_added << u
           Blog.info "Update roles: Added to employees: #{u.id} - #{u.email}"
