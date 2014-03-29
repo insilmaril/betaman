3ac9094d3957ae76b2a77e56b04ebcfb030f4fb6
Uwe Drechsel <uwedr@suse.com>
2014-03-29 09:12:50 +0100

    Order diary entries for user with latest on top

--- app/controllers/admin/diary_controller.rb
+++ app/controllers/admin/diary_controller.rb
@@ -5,7 +5,7 @@ class Admin::DiaryController < ApplicationController
 
   def user
     @user = User.find(params[:user_id])
-    @diary_entries = DiaryEntry.where('(user_id = ?) OR (actor_id = ?)', @user.id, @user.id)
+    @diary_entries = DiaryEntry.where('(user_id = ?) OR (actor_id = ?)', @user.id, @user.id).order('created_at DESC')
   end
 end
 
