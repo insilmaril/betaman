eef94f9e3a89dca3022ede67615468805cfe9e99
Uwe Drechsel <uwedr@suse.com>
2014-08-05 14:32:49 +0200

    Basic pagination for diary view

--- app/controllers/admin/diary_controller.rb
+++ app/controllers/admin/diary_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DiaryController < ApplicationController
   def index
-    @diary_entries = DiaryEntry.order('created_at DESC')
+    @diary_entries = DiaryEntry.order('created_at DESC').page(params[:page]).per_page(7)
   end
 
   def user
