0b5d5f0ed76a892d30c88252dcc17cfa8b1c7618
Uwe Drechsel <uwedr@suse.com>
2014-10-27 13:51:36 +0100

    Added minimal script to unsubscribe externals from current lists

---
+++ unsubscribe-externals.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env script/rails runner
+
+require 'betaadmin'
+require 'download_helper'
+
+begin
+  puts "Unbscribing external users of Betas with downloads..."
+
+  betas_with_downloads = DownloadHelper.find_betas_with_downloads
+  betas_with_downloads.each do |b|
+    puts "* #{b[:beta].name}"
+
+    users = b[:beta].list.users
+
+    n = 1
+    users.each do |u|
+      if ! u.employee?
+        puts "    Unsubscribing (#{n}/#{users.count}): #{u.logname}"
+        b[:beta].list.unsubscribe(u)
+        n += 1
+      end
+    end
+    
+    #puts "    Sync external list to internal..."
+    #b[:beta].list.sync_to_intern
+  end
+end
